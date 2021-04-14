b82234510de8724b7968135007f38cc05dbec4ce
Elizaveta Morozova <rusquee@gmail.com>
2021-04-14 12:13:58 +0300

    Produce jacoco branch coverage

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -8,16 +8,16 @@ class RadioTest {
     @Test
     public void shouldGetAndSetStation() {
         Radio radio = new Radio();
-        radio.setCurrentRadioStation(-1);
-        int expected = 9;
+        radio.setCurrentRadioStation(8);
+        int expected = 8;
         assertEquals(expected, radio.getCurrentRadioStation());
     }
 
     @Test
     public void shouldGetAndSetVolume() {
         Radio radio = new Radio();
-        radio.setVolume(11);
-        int expected = 10;
+        radio.setVolume(5);
+        int expected = 5;
         assertEquals(expected, radio.getVolume());
     }
 
@@ -31,11 +31,47 @@ class RadioTest {
     }
 
     @Test
+    public void shouldSwitchToNextStationIfLimit() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(9);
+        radio.switchToNextStation();
+        int expected = 0;
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldSwitchToNextStationOverLimit() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(20);
+        radio.switchToNextStation();
+        int expected = 1;
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+
+    @Test
     public void shouldSwitchToPrevStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(6);
+        radio.switchToPrevStation();
+        int expected = 5;
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldSwitchToPrevStationIfLimit() {
         Radio radio = new Radio();
         radio.setCurrentRadioStation(0);
         radio.switchToPrevStation();
         int expected = 9;
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldSwitchToPrevStationUnderLimit() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(-5);
+        radio.switchToPrevStation();
+        int expected = 8;
         assertEquals(expected, radio.getCurrentRadioStation());
     }
 
@@ -49,9 +85,36 @@ class RadioTest {
     }
 
     @Test
+    public void shouldIncreaseVolumeOverLimit() {
+        Radio radio = new Radio();
+        radio.setVolume(20);
+        radio.increaseVolume();
+        int expected = 10;
+        assertEquals(expected, radio.getVolume());
+    }
+
+    @Test
     public void shouldDecreaseVolume() {
         Radio radio = new Radio();
+        radio.setVolume(4);
+        radio.decreaseVolume();
+        int expected = 3;
+        assertEquals(expected, radio.getVolume());
+    }
+
+    @Test
+    public void shouldDecreaseVolumeIfLimit() {
+        Radio radio = new Radio();
         radio.setVolume(0);
+        radio.decreaseVolume();
+        int expected = 0;
+        assertEquals(expected, radio.getVolume());
+    }
+
+    @Test
+    public void shouldDecreaseVolumeUnderLimit() {
+        Radio radio = new Radio();
+        radio.setVolume(-1);
         radio.decreaseVolume();
         int expected = 0;
         assertEquals(expected, radio.getVolume());
